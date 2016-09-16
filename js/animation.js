867e9dbac566609f02e4c47ce43525ec63814f2b
Talon Hughes <talon.hughes@gmail.com>
2016-09-16 17:01:07 -0600

    completed step 6and7

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 
-/*The Tweet button and the char counter are initially hidden. If the user clicks on the textarea, the textarea doubles in size and the char counter and Tweet button are revealed.*/
-    $('.tweet-compose').on('focusin', function() {
+    //Hides Tweet Button and Character Count
+    $('#tweet-controls').hide();
+
+    //Textarea Doubles
+    /*The Tweet button and the char counter are initially hidden. If the user clicks on the textarea, the textarea doubles in size and the char counter and Tweet button are revealed.*/
+    var doubleSize = $('.tweet-compose').on('focusin', function() {
         $(this).animate({
-            height: "6em"
+            height: '5em'
         }, 500);
         $('#tweet-controls').show();
     });
 
-/*As the user types the char counter decreases. Once it hits 10 characters or less the counter turns red. If the user enters more than 140 chars, the Tweet button is disabled (and re-enabled when there are <= 140 chars)*/
-$('.tweet-compose').on('keyup keydown keypress', function() {
+
+    //Character Counter
+    /*As the user types the char counter decreases. Once it hits 10 characters or less the counter turns red. If the user enters more than 140 chars, the Tweet button is disabled (and re-enabled when there are <= 140 chars)*/
+    $('.tweet-compose').on('keyup keydown keypress', function() {
         var newCount = 140 - $(this).val().length;
         $('#char-count').text(newCount);
         if (newCount < 0) {
@@ -26,71 +32,70 @@ $('.tweet-compose').on('keyup keydown keypress', function() {
         }
     });
 
-    /*When the user successfully inputs characters and clicks the “Tweet” button, a new tweet should be created and added to the tweet stream in the main column, using the user’s fake profile image in the top left and username/fullname. -->
-    <!-- HINT: jQuery ".prepend" method*/
+    //Adds New Tweet
+    /*When the user successfully inputs characters and clicks the “Tweet” button, a new tweet is created and added to the tweet stream in the main column, using the user’s fake profile image in the top left and username/fullname. */
 
 
-$('#tweet-submit').on('click', function() {
-    var newTweet = $('.tweet-compose').val();
-    $("#tweet-controls").hide();
-    $('.tweet-compose').animate({height: "2.5em"});
-    $('#stream').prepend(
-        '<div class="tweet">' +
-        '<div class="content">' +
-        '<img class="avatar" src="img/dogecream.jpg" />' +
-        '<strong class="fullname">dogecream</strong>' +
-        '<span class="username">@dogecream</span>' +
-        '<p class="tweet-text">' + newTweet + '</p>' +
-        '<div class="tweet-actions">' +
-        '<ul>' +
-        '<li><span class="icon action-reply"></span> Reply</li>' +
-        '<li><span class="icon action-favorite"></span> Favorite</li>' +
-        '<li><span class="icon action-more"></span> More</li>' +
-        '</ul>' +
-        '</div>' +
-        '<div class="stats-reply-container">' +
-        '<div class="stats">' +
-        '<div class="retweets">' +
-        '<p class="num-retweets">30</p>' +
-        '<p>RETWEETS</p>' +
-        '</div>' +
-        '<div class="favorites">' +
-        '<p class="num-favorites">6</p>' +
-        '<p>FAVORITES</p>' +
-        '</div>' +
-        '<div class="users-interact">' +
-        '<div>' +
-        '<img src="img/memyself.png" />' +
-        '<img src="img/vklimenko.jpg" />' +
-        '</div>' +
-        '</div>' +
-        '<div class="time">' +
-        '1:04 PM - 19 Sep 13' +
-        '</div>' +
-        '</div>' +
-        '<div class="reply">' +
-        '<img class="avatar" src="img/memyself.png" />' +
-        '<textarea class="tweet-compose" placeholder="Reply to @dogecream"/></textarea>' +
-        '</div>' +
-        '</div>' +
-        '</div>');
 
-    $('.tweet-actions ul').hide();
-    $('.tweet').on('mouseenter', function() {
-        $(this).find('ul').slideDown();
+    $('#tweet-submit').on('click', function() {
+        var newTweet = $('.tweet-compose').val();
+        $("#tweet-controls").hide();
+        $('.tweet-compose').animate({
+            height: "2.5em"
+        });
+        $('#stream').prepend(
+            '<div class="tweet">' +
+            '<div class="content">' +
+            '<img class="avatar" src="img/memyself.png" />' +
+            '<strong class="fullname">Talon Hughes</strong>' +
+            '<span class="username"> @thughes</span>' +
+            '<p class="tweet-text">' + newTweet + '</p>' +
+            '<div class="tweet-actions">' +
+            '<ul>' +
+            '<li><span class="icon action-reply"></span> Reply</li>' +
+            '<li><span class="icon action-favorite"></span> Favorite</li>' +
+            '<li><span class="icon action-more"></span> More</li>' +
+            '</ul>' +
+            '</div>' +
+            '<div class="stats-reply-container">' +
+            '<div class="stats">' +
+            '<div class="retweets">' +
+            '<p class="num-retweets">30</p>' +
+            '<p>RETWEETS</p>' +
+            '</div>' +
+            '<div class="favorites">' +
+            '<p class="num-favorites">6</p>' +
+            '<p>FAVORITES</p>' +
+            '</div>' +
+            '<div class="users-interact">' +
+            '<div>' +
+            '<img src="img/memyself.png" />' +
+            '<img src="img/vklimenko.jpg" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="time">' +
+            '1:04 PM - 19 Sep 13' +
+            '</div>' +
+            '</div>' +
+            '<div class="reply">' +
+            '<img class="avatar" src="img/memyself.png" />' +
+            '<textarea class="tweet-compose" placeholder="Reply to @dogecream"/></textarea>' +
+            '</div>' +
+            '</div>' +
+            '</div>');
+
+        $('.tweet-compose').val('');
+        $(this).find('.tweet-actions').hide();
+
+        $('.stats').hide();
+        $('.reply').hide();
+        //Reveals Retweets, Favorites, and Time Stamp
+        $('.tweet').on('click', function() {
+            $(this).find('.stats').toggle('fast', 'linear');
+            $(this).find('.reply').toggle('fast', 'linear');
+        });
+
     });
-    $('.tweet').on('mouseleave', function() {
-        $(this).find('ul').slideUp();
-    });
-
-    $('.stats-reply-container').hide();
-    $('.tweet').on('click', function() {
-        $(this).find('.stats-reply-container').animate({height:'toggle', opacity:100});
-    });
-    newTweet = $('.tweet-compose').val("");
-});
-
-
 
 
 
