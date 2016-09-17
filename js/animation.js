69e52d4a636e9c4e99ab19571261e6d941f3130f
Talon Hughes <talon.hughes@gmail.com>
2016-09-17 14:02:17 -0600

    finished project

--- js/animation.js
+++ js/animation.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
 
-    //Hides Tweet Button and Character Count
-    $('#tweet-controls').hide();
+    var userName = 'Talon Hughes';
+    var myHandle = ' @thughes';
+    $('#user-name').text(userName);
+    $('#handle').text(myHandle);
 
-    //Textarea Doubles
-    /*The Tweet button and the char counter are initially hidden. If the user clicks on the textarea, the textarea doubles in size and the char counter and Tweet button are revealed.*/
-    var doubleSize = $('.tweet-compose').on('focusin', function() {
+    //timeago Plugin
+    $("time.timeago").timeago();
+
+    // Bootstrap Tooltips
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    //Hides Tweet Button and Character Count
+    $('#tweet-controls').css('display', 'none');
+
+    //When the user clicks on the textarea, the textarea doubles in size and the Character Counter and Tweet button are revealed.
+    var doubleSize = $('.tweet-compose').on('click', function() {
         $(this).animate({
             height: '5em'
         }, 500);
-        $('#tweet-controls').show();
+        $('#tweet-controls').css('display', 'inherit');
     });
 
 
     //Character Counter
-    /*As the user types the char counter decreases. Once it hits 10 characters or less the counter turns red. If the user enters more than 140 chars, the Tweet button is disabled (and re-enabled when there are <= 140 chars)*/
+
+    //As the user types the char counter decreases.
     $('.tweet-compose').on('keyup keydown keypress', function() {
         var newCount = 140 - $(this).val().length;
         $('#char-count').text(newCount);
+
+        //If the user puts in more than 140 characters, the tweet button disables and the negative numbers fade to a lighter red.
         if (newCount < 0) {
             $('#char-count').css('opacity', '0.4').css('color', '#D70F0F');
             $('#tweet-submit').attr('disabled', true);
@@ -25,6 +40,8 @@ $(document).ready(function() {
             $("#char-count").css('opacity', '1').css('color', '#999');
             $('#tweet-submit').removeAttr('disabled');
         }
+
+        //When there are 10 or less characters, the characters turn red.
         if (newCount <= 10) {
             $('#char-count').css('color', '#D70F0F');
         } else {
@@ -32,10 +49,12 @@ $(document).ready(function() {
         }
     });
 
+
+
+
     //Adds New Tweet
+
     /*When the user successfully inputs characters and clicks the “Tweet” button, a new tweet is created and added to the tweet stream in the main column, using the user’s fake profile image in the top left and username/fullname. */
-
-
 
     $('#tweet-submit').on('click', function() {
         var newTweet = $('.tweet-compose').val();
@@ -43,12 +62,13 @@ $(document).ready(function() {
         $('.tweet-compose').animate({
             height: "2.5em"
         });
+
         $('#stream').prepend(
             '<div class="tweet">' +
             '<div class="content">' +
             '<img class="avatar" src="img/memyself.png" />' +
-            '<strong class="fullname">Talon Hughes</strong>' +
-            '<span class="username"> @thughes</span>' +
+            '<strong class="fullname">' + userName + '</strong>' +
+            '<span class="username">' + myHandle + '</span>' +
             '<p class="tweet-text">' + newTweet + '</p>' +
             '<div class="tweet-actions">' +
             '<ul>' +
@@ -58,7 +78,7 @@ $(document).ready(function() {
             '</ul>' +
             '</div>' +
             '<div class="stats-reply-container">' +
-            '<div class="stats">' +
+            '<div class="stats toggle">' +
             '<div class="retweets">' +
             '<p class="num-retweets">30</p>' +
             '<p>RETWEETS</p>' +
@@ -74,10 +94,11 @@ $(document).ready(function() {
             '</div>' +
             '</div>' +
             '<div class="time">' +
-            '1:04 PM - 19 Sep 13' +
+            // '1:04 PM - 19 Sep 13' +
+            $.timeago(new Date()) +
             '</div>' +
             '</div>' +
-            '<div class="reply">' +
+            '<div class="reply toggle">' +
             '<img class="avatar" src="img/memyself.png" />' +
             '<textarea class="tweet-compose" placeholder="Reply to @dogecream"/></textarea>' +
             '</div>' +
@@ -85,24 +106,15 @@ $(document).ready(function() {
             '</div>');
 
         $('.tweet-compose').val('');
-        $(this).find('.tweet-actions').hide();
-
-        $('.stats').hide();
-        $('.reply').hide();
-        //Reveals Retweets, Favorites, and Time Stamp
-        $('.tweet').on('click', function() {
-            $(this).find('.stats').show();
-            $(this).find('.reply').show();
-        });
-
+        $('#char-count').html(140);
+        
     });
 
-
-
-
-
-
-
-
+    //Hides Retweets, Favorites, Time Stamp, and Reply textarea.
+    $('.toggle').hide();
+    //Reveals Retweets, Favorites, and Time Stamp.
+    $('.tweet').on('click', function() {
+        $(this).find('.toggle').slideToggle();
+    });
 
 });
